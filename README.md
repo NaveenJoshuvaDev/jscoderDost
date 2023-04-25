dec58dddf5c820d59442bcf68d96f8c93a21b1da
NaveenJoshuvaDev <njworkholic@gmail.com>
2023-04-25 13:33:08 +0530

    string methods added

--- README.md
+++ README.md
@@ -14,3 +14,28 @@ let fullName = firstName + " " + lastName;
 let fullName = `I want to Become ${firstName} ${lastName}`
 
 ```
+- Getting String Character
+```
+console.log(firstName[2])
+
+```
+
+- String Methods
+```
+console.log(firstName.toLocaleLowerCase())
+console.log(firstName.toLocaleUpperCase())
+console.log(firstName.toLowerCase())
+console.log(lastName.indexOf("g"))
+
+```
+
+- Common Strings Methods
+```
+let hobbies = '   code read run';
+let result = hobbies.trim()
+console.log(hobbies);
+console.log(result);
+
+```
+** trim method **
+- used to delete the spaces
--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 let firstName = "wow";
 let lastName = "Programmer";
 let fullName = firstName + " " + lastName;
+//Getting String Character
+console.log(firstName[2])
+//String Methods
+console.log(firstName.toLocaleLowerCase())
+console.log(firstName.toLocaleUpperCase())
+console.log(firstName.toLowerCase())
+console.log(lastName.indexOf("g"))
+// Common Strings Methods
+
+let hobbies = 'code read run';
+
+//trim method
+let result = hobbies.trim()
+console.log(hobbies);
+console.log(result);
+//used to delete the spaces
+//indexof 
+console.log(result.indexOf("code"))
+console.log(result.lastIndexOf("run"))
